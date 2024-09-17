00f16b12d7f14e216bfbbd7cc60919eb5d4d5ada
Zhijiang Chen <zhijiang@chen.me>
2024-09-17 18:54:21 -0400

    Project init.

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+
+    // Add method
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    // Subtract method
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+    // Multiply method
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    // Divide method
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+}
